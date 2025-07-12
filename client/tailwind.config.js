adadad2d8a8955a6732d3a2dbbc634bdf97657bd
Prasant Kumar Mahato <prasantmahato33@gmail.com>
2025-07-12 23:31:38 +0530

    Updated Add Task modal with backgroud blur

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/*.{js,ts,jsx,tsx}'],
     theme: {
-      extend: {},
+      extend: {
+        animation: {
+            fadeIn: 'fadeIn 0.2s ease-out forwards',
+          },
+          keyframes: {
+            fadeIn: {
+              from: { opacity: 0, transform: 'scale(0.95)' },
+              to: { opacity: 1, transform: 'scale(1)' },
+            },
+          },
+      },
     },
     plugins: [],
   }
